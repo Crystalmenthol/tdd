e49388ac875feda6d9233bdb20c305653d2a748c
crystalmenthol <wutwut@odds.team>
2022-01-26 14:44:58 +0700

    remove unused

--- roman/roman.go
+++ roman/roman.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 )
 
-type Roman interface {
-	ToRoman(n int) string
-}
-
 var num = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
 var code = []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 
