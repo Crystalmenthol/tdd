87e65d38c8524d7ec7719f5d042f7c4688b2be03
crystalmenthol <wutwut@odds.team>
2022-01-26 14:31:40 +0700

    rewrite test

--- fizzbuzz/fizzbuzz_test.go
+++ fizzbuzz/fizzbuzz_test.go
@@ -1,13 +1,47 @@
 package fizzbuzz
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestFizzBuzz(t *testing.T) {
-	t.Run("test ", func(t *testing.T) {
-		result := FizzBuzz(33)
-		assert.Equal(t, "Fizz", result)
+	t.Run("test FizzBuzz with 1 should get 1", func(t *testing.T) {
+		expected := "1"
+
+		actual := FizzBuzz(1)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
+	})
+
+	t.Run("test FizzBuzz with 3 should get Fizz", func(t *testing.T) {
+		expected := "Fizz"
+
+		actual := FizzBuzz(3)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
+	})
+
+	t.Run("test FizzBuzz with 5 should get Buzz", func(t *testing.T) {
+		expected := "Buzz"
+
+		actual := FizzBuzz(5)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
+	})
+
+	t.Run("test FizzBuzz with 15 should get FizzBuzz", func(t *testing.T) {
+		expected := "Fizz Buzz"
+
+		actual := FizzBuzz(15)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
 	})
 }
--- roman/roman_test.go
+++ roman/roman_test.go
@@ -1,15 +1,58 @@
 package roman
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestToRoman(t *testing.T) {
 
-	t.Run("expect 9", func(t *testing.T) {
-		result := ToRoman(9)
+	t.Run("test ToRoman with 4", func(t *testing.T) {
+		expected := "IV"
 
-		assert.Equal(t, "IX", result)
+		actual := ToRoman(4)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
+	})
+
+	t.Run("test ToRoman with 9", func(t *testing.T) {
+		expected := "IX"
+
+		actual := ToRoman(9)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
+	})
+
+	t.Run("test ToRoman with 10", func(t *testing.T) {
+		expected := "X"
+
+		actual := ToRoman(10)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
+	})
+
+	t.Run("test ToRoman with 19", func(t *testing.T) {
+		expected := "XIX"
+
+		actual := ToRoman(19)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
+	})
+
+	t.Run("test ToRoman with 40", func(t *testing.T) {
+		expected := "XL"
+
+		actual := ToRoman(40)
+
+		if actual != expected {
+			t.Errorf("expected %v, but got %v ", expected, actual)
+		}
 	})
 }
